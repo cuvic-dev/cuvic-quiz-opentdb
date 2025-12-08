8ec7fdf0140156d996d3b536f128e7fdda9ff6ed
Cuvic Dev <cuvic.development@gmail.com>
2025-12-08 18:31:44 +0800

    added decodehtml utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function decodeHtml(html: string) {
+  return html.replace(/&#(\d+);/g, (m, code) =>
+    String.fromCharCode(Number(code))
+  ).replace(/&quot;/g, '"')
+   .replace(/&apos;/g, "'")
+   .replace(/&amp;/g, '&')
+   .replace(/&lt;/g, '<')
+   .replace(/&gt;/g, '>');
+}
