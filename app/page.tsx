959887f9ea2819dfa2fb5054912c3fe91e0de1ff
Cuvic Dev <cuvic.development@gmail.com>
2025-12-09 16:25:51 +0800

    redirect to quiz page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-medium">Main Page</h1>
-    </div>
-  );
+  redirect("/quiz");
 }
